0d73c41f2d6441db63a0632b99255cfbaa2c402d
Chiedza <yevisochiedza@gmail.com>
2025-09-26 13:02:23 +0100

    Updating the system with a network bridge

--- src/main/java/com/napier/sem/Main.java
+++ src/main/java/com/napier/sem/Main.java
@@ -11,7 +11,7 @@ public class Main
     public static void main(String[] args)
     {
         // Connect to MongoDB on local system - we're using port 27000
-        try(MongoClient mongoClient = new MongoClient("mongo-dbserver")){
+        try(MongoClient mongoClient = new MongoClient("mongo")){
             // Get a database - will create when we use it
             MongoDatabase database = mongoClient.getDatabase("mydb");
             // Get a collection from the database
