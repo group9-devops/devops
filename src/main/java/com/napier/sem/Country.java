aa01f450c5fbdf5a2844e8675e83fc27024b7fad
euanb <euan.birk@gmail.com>
2025-10-10 11:37:51 +0100

    Creating class files for DB record viewing

--- src/main/java/com/napier/sem/Country.java
+++ src/main/java/com/napier/sem/Country.java
@@ -8,10 +8,28 @@ import java.sql.Connection;
  * Represents a Country
  */
 public class Country {
-    public int code;
+    /**
+     * Country's Code
+     */
+    public String code;
+    /**
+     * Country's name
+     */
     public String name;
+    /**
+     * Country's continent
+     */
     public String continent;
+    /**
+     * Country's region
+     */
     public String region;
+    /**
+     * Country's population
+     */
     public int population;
+    /**
+     * Country's capital
+     */
     public String capital;
 }
