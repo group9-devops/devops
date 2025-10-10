8ab647902e14b0a4318c39498800d14a948a85ee
euanb <euan.birk@gmail.com>
2025-10-10 11:32:37 +0100

    Creating class files for DB record viewing

--- src/main/java/com/napier/sem/City.java
+++ src/main/java/com/napier/sem/City.java
@@ -1,4 +1,16 @@
 package com.napier.sem;
 
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.Connection;
+
+/**
+ * Represents a City
+ */
 public class City {
+    public String name;
+    public String country;
+    public String district;
+    public int population;
+    public boolean isCapital;
 }
--- src/main/java/com/napier/sem/Country.java
+++ src/main/java/com/napier/sem/Country.java
@@ -1,5 +1,17 @@
 package com.napier.sem;
 
-public class Country {
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.Connection;
 
+/**
+ * Represents a Country
+ */
+public class Country {
+    public int code;
+    public String name;
+    public String continent;
+    public String region;
+    public int population;
+    public String capital;
 }
