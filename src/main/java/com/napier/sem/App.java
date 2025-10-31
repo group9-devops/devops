5cc31678f1d087712a918dc4f976a552ccb86336
tulsirijal2019@gmail.com <tulsirijal@2019>
2025-10-31 12:44:09 +0000

    add new java class to create country report

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -70,33 +70,6 @@ public class App
             }
         }
     }
-    public ArrayList<Country> getCountry()
-    {
-        try {
-            Statement stmt = con.createStatement();
-            String sqlStatement =  "SELECT * FROM country ORDER BY population DESC";
-            ResultSet rset = stmt.executeQuery(sqlStatement);
-            ArrayList<Country> countries = new ArrayList<Country>();
-            while(rset.next()){
-                Country country = new Country();
-                country.code = rset.getString("Code");
-                country.name = rset.getString("Name");
-                country.continent = rset.getString("Continent");
-                country.population = rset.getInt("Population");
-                country.capital = rset.getString("Capital");
-                country.region = rset.getString("Region");
-                countries.add(country);
-            }
-
-            return countries;
-
-        }
-        catch (Exception e){
-            System.out.println(e.getMessage());
-            System.out.println("Failed to get country");
-            return null;
-        }
-    }
 
 
     public static void main(String[] args)
@@ -106,24 +79,10 @@ public class App
 
         // Connect to database
         a.connect();
-        ArrayList<Country> countries = a.getCountry();
+        PrintCountryValues PrintCountry = new PrintCountryValues();
+        PrintCountry.getAllCountriesByPopulationDescending(a.con);
 
-        // format of the table
-        System.out.printf("%-30s %-20s %-15s %-20s %-30s %15s\n",
-                "Name", "Continent", "Region", "Capital", "Code", "Population");
-        System.out.println("--------------------------------------------------------------------------------------------------------------");
 
-// Print rows
-        for (Country country : countries) {
-            System.out.printf("%-30s %-20s %-15s %-20s %-30s %,15d\n",
-                    country.name,
-                    country.continent,
-                    country.region,
-                    country.capital,
-                    country.code,
-                    country.population
-            );
-        }
 
         // Disconnect from database
         a.disconnect();
