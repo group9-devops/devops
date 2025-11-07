4678bc08e859f2810f63dc0dc3dc8e6d7dbe7163
Chiedza <yevisochiedza@gmail.com>
2025-11-07 11:43:05 +0000

    Updated the GitHub workflows

---
+++ src/test/java/com/napier/devops/AppTest.java
@@ -0,0 +1,21 @@
+package com.napier.devops;
+
+import com.napier.sem.App;
+import com.napier.sem.Country;
+import com.napier.sem.City;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    static App app;
+
+    @BeforeAll
+    static void init() {
+        app = new App();
+    }
+}
