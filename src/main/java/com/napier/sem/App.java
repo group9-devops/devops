5ced5fb8305f04145059462d2b23026015480f8f
Chiedza <yevisochiedza@gmail.com>
2025-11-11 00:35:30 +0000

    Enabling local debugging

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -18,41 +18,32 @@ public class App
      * Establishes a connection to the MySQL database.
      * Retries up to 10 times, waiting 30 seconds between attempts.
      */
-    public void connect()
-    {
-        try
-        {
+    public void connect(String location, int delay) {
+        try {
+            // Load Database driver
             Class.forName("com.mysql.cj.jdbc.Driver");
-        }
-        catch (ClassNotFoundException e)
-        {
+        } catch (ClassNotFoundException e) {
             System.out.println("Could not load SQL driver");
             System.exit(-1);
         }
 
         int retries = 10;
-        for (int i = 0; i < retries; ++i)
-        {
+        for (int i = 0; i < retries; ++i) {
             System.out.println("Connecting to database...");
-            try
-            {
-                Thread.sleep(30000);
-                con = DriverManager.getConnection(
-                        "jdbc:mysql://db:3306/world?allowPublicKeyRetrieval=true&useSSL=false",
-                        "root",
-                        "example"
-                );
-                System.out.println("Connection established!");
+            try {
+                // Wait a bit for db to start
+                Thread.sleep(delay);
+                // Connect to database
+                con = DriverManager.getConnection("jdbc:mysql://" + location
+                                + "/world?allowPublicKeyRetrieval=true&useSSL=false",
+                        "root", "example");
+                System.out.println("Successfully connected");
                 break;
-            }
-            catch (SQLException sqle)
-            {
-                System.out.println("Connection failed. Attempt: " + i);
+            } catch (SQLException sqle) {
+                System.out.println("Failed to connect to database attempt " +                                  Integer.toString(i));
                 System.out.println(sqle.getMessage());
-            }
-            catch (InterruptedException ie)
-            {
-                System.out.println("Thread interrupted unexpectedly.");
+            } catch (InterruptedException ie) {
+                System.out.println("Thread interrupted? Should not happen.");
             }
         }
     }
@@ -86,8 +77,14 @@ public class App
         // Create new Application
         App a = new App();
 
+        if(args.length < 1){
+            a.connect("localhost:3307", 30000);
+        }else{
+            a.connect(args[0], Integer.parseInt(args[1]));
+        }
+
         // Connect to database
-        a.connect();
+
         PrintCountryValues PrintCountry = new PrintCountryValues();
         PrintCountry.getAllCountriesByPopulationDescending(a.con);
 
